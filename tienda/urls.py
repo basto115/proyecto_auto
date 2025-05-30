e7769d5f51a55ff9e041b2326b3d5d960f60e11a
jotakaii <jose.duran@duocuc.cl>
2025-05-29 23:41:52 -0400

    nose

--- tienda/urls.py
+++ tienda/urls.py
@@ -2,7 +2,13 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
+<<<<<<< Updated upstream
     path('', views.home, name="home"),
     path('', views.lista_productos, name='lista_productos'),
 ]
+=======
+    path('', views.lista_productos, name='lista_productos'),
+]
+>>>>>>> Stashed changes
--- tienda/views.py
+++ tienda/views.py
@@ -2,12 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Producto
 
+<<<<<<< Updated upstream
 # Create your views here.
 
 def home(request):
     return render(request, 'tienda/index.html')
+=======
+from django.http import HttpResponse
+from .models import Producto
+
+def inicio(request):
+    return HttpResponse("¡Bienvenido a AutoParts!")
+
+
+>>>>>>> Stashed changes
 
 def lista_productos(request):
     productos = Producto.objects.all()
     return render(request, 'tienda/lista_productos.html', {'productos': productos})
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
