9b2b2443ff4c641f48efd7252d415629a55d6c41
jotakaii <jose.duran@duocuc.cl>
2025-05-29 23:47:35 -0400

    ya no topa nada lol

--- tienda/urls.py
+++ tienda/urls.py
@@ -4,11 +4,6 @@ from . import views
 
 
 urlpatterns = [
-<<<<<<< Updated upstream
     path('', views.home, name="home"),
     path('', views.lista_productos, name='lista_productos'),
 ]
-=======
-    path('', views.lista_productos, name='lista_productos'),
-]
->>>>>>> Stashed changes
