38960f43c055a8a92cfcf6af180f6673892f7b23
jotakaii <jose.duran@duocuc.cl>
2025-06-01 00:33:17 -0400

    se habian duplicado los urls lol

--- tienda/urls.py
+++ tienda/urls.py
@@ -1,36 +1,35 @@
 
 from django.urls import path
 from . import views
-from .views import realizar_pedido
-from .views import login_view, register_view
-
+from .views import realizar_pedido, login_view, register_view
 
 urlpatterns = [
     path('', views.home, name="home"),
     path('catalogo/', views.catalogo, name='catalogo'),
-    path('blog', views.blog, name="blog"),
-    path('login', views.login, name="login"),
-    path('checkout', views.checkout, name="checkout"),
-    path('confirmation', views.confirmation, name="confirmation"),
-    path('single_blog', views.single_blog, name="single_blog"),
+    path('blog/', views.blog, name="blog"),
     path('login/', login_view, name="login"),
-    path('tracking', views.tracking, name="tracking"),
-    path('contact', views.contact, name="contact"),
+    path('register/', register_view, name='register'),
+    path('checkout/', views.checkout, name="checkout"),
+    path('confirmation/', views.confirmation, name="confirmation"),
+    path('single_blog/', views.single_blog, name="single_blog"),
+    path('tracking/', views.tracking, name="tracking"),
+    path('contact/', views.contact, name="contact"),
+
     path('carrito/', views.ver_carrito, name='ver_carrito'),
     path('realizar-pedido/', realizar_pedido, name='realizar_pedido'),
     path('agregar/<int:producto_id>/', views.agregar_producto, name='agregar_producto'),
     path('eliminar/<int:producto_id>/', views.eliminar_producto, name='eliminar_producto'),
     path('vaciar/', views.vaciar_carrito, name='vaciar_carrito'),
-    path('register/', register_view, name='register'),
-    path('checkout/', views.checkout, name='checkout'),
+
     path('confirmacion/', views.confirmacion_pago, name='confirmacion_pago'),
-    path('pedidos-bodeguero/', views.pedidos_bodeguero, name='pedidos_bodeguero'),
-    path('pedidos-bodeguero/<int:pedido_id>/recolectado/', views.marcar_recolectado, name='marcar_recolectado'),
-    path('pedidos-repartidor/', views.pedidos_repartidor, name='pedidos_repartidor'),
-    path('pedidos-repartidor/<int:pedido_id>/entregado/', views.marcar_entregado, name='marcar_entregado'),
+    path('producto/<int:producto_id>/', views.detalle_producto, name='detalle_producto'),
+
+    # Bodeguero
     path('pedidos/bodeguero/', views.pedidos_bodeguero, name='pedidos_bodeguero'),
     path('pedidos/bodeguero/<int:pedido_id>/recolectado/', views.marcar_recolectado, name='marcar_recolectado'),
+
+    # Repartidor
     path('pedidos/repartidor/', views.pedidos_repartidor, name='pedidos_repartidor'),
     path('pedidos/repartidor/<int:pedido_id>/entregado/', views.marcar_entregado, name='marcar_entregado'),
-    path('producto/<int:producto_id>/', views.detalle_producto, name='detalle_producto'),
 ]
+
